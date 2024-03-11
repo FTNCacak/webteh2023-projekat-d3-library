0c59983455e6bdec08dd2796de6628f393b56525
Pavle Milanović <pavlemilanovicgim11111@gmail.com>
2024-03-11 14:23:55 +0100

    Navbar početak

--- src/views/partials/Navbar.jsx
+++ src/views/partials/Navbar.jsx
@@ -1,8 +1,29 @@
+import "./Navbar.scss";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 function Navbar() {
     return (
-        <div>
-            navbar
-        </div>
+        <header>
+            <nav>
+                <ul className="page-links">
+                    <li className="link"><a href="#">Home</a></li>
+                    <li className="link"><a href="#">X</a></li>
+                    <li className="link"><a href="#">X</a></li>
+                    <li className="link"><a href="#">X</a></li>
+                    <li className="link"><a href="#">X</a></li>
+                    <li className="link"><a href="#">X</a></li>
+                    <li className="link">
+                        <FontAwesomeIcon className="youtube" icon={faYoutube} />
+                    </li>
+                    <li className="link">
+                        <div className="translation-buttons">translate</div>
+                    </li>
+                </ul>
+            </nav>
+            
+        </header>
     );
 }
 
