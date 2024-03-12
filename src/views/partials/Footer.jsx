7265aa8c7d93cf0feb7f6ea93af1c296bcef82a4
Aleksandar Arnautovic <aleksandararnautovic28@gmail.com>
2024-03-12 14:35:58 +0100

    Footer

--- src/views/partials/Footer.jsx
+++ src/views/partials/Footer.jsx
@@ -1,8 +1,49 @@
+import "./Footer.scss";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 function Footer() {
     return (
-        <div>
-            footer
-        </div>
+        <footer >
+            <div className="footer-row">
+                <div className="container">
+                    <div className="row">
+                        <div className="col col-md-3">
+                            "X" <br /> "Y"
+                        </div>
+                        <div className="col col-md-3 m-top-sm-m">
+                            "X@.it" <br />
+                        </div>
+                        <div className="col col-md-3 m-top-sm-m">
+                            "t." <br /> "f."
+                        </div>
+                        <div className="col col-md-3 align-right align-central-sm m-top-sm-m">
+                                <FontAwesomeIcon className="facebook" icon={faFacebook} />
+                                <FontAwesomeIcon className="instagram" icon={faInstagram} />
+                        </div>
+                    </div>
+                </div>
+            </div> 
+            <div className="footer-row1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col col-md-3">
+                            "X" <br /> "Y"
+                        </div>
+                        <div className="col col-md-3 m-top-sm-m">
+                            "X@.it" <br />
+                        </div>
+                        <div className="col col-md-3 m-top-sm-m">
+                            
+                        </div>
+                        <div className="col col-md-3 align-right align-central-sm m-top-sm-m">
+                                "x"
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </footer>
     );
 }
 
